2c1b9e01559f9142a27d6d17acd0d0196fa66a23
PyMinna <kadeejaminnna@gmail.com>
2026-07-07 10:59:58 +0530

    area of circle example

---
+++ Practical/area.py
@@ -0,0 +1,7 @@
+import math_tools
+
+radius = float(input("Enter the radius: "))
+
+area = math_tools.area_of_circle(radius)
+
+print("Area of the circle is: ",area)
---
+++ Practical/math_tools.py
@@ -0,0 +1,5 @@
+import math
+
+def area_of_circle(radius):
+    area = math.pi * radius ** 2
+    return area
