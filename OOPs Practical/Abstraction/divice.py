b5c24af20eec0dc4955251ce9c26946a3be43791
PyMinna <kadeejaminnna@gmail.com>
2026-07-14 17:24:22 +0530

    hiding internal steps inside a method example

---
+++ OOPs Practical/Abstraction/divice.py
@@ -0,0 +1,20 @@
+from abc import ABC,abstractmethod
+
+class Divice(ABC):
+    @abstractmethod
+    def boot(self):
+        pass
+
+class MyDivice(Divice):
+    def boot(self):
+        self.__load_os()
+        self.__checking_hardware()
+
+    def __load_os(self):
+        print("Os loaded...")
+
+    def __checking_hardware(self):
+        print("hardware checked..")
+
+d =MyDivice()
+d.boot()
