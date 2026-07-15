5eee6ab5b32afe25ec5978f1ae41aedd2f267933
PyMinna <kadeejaminnna@gmail.com>
2026-07-15 15:48:49 +0530

    pdfreader and imageviewer polymorphism

---
+++ OOPs Practical/Polymorphism/pdfreader_polymorphism.py
@@ -0,0 +1,15 @@
+class PDFreader:
+    def open(self):
+        return "Read poem.pdf"
+    
+class ImageViewer:
+    def open(self):
+        return "View sky.png"
+    
+def view_pdf(view):
+    print(view.open())
+
+p = PDFreader()
+i = ImageViewer()
+view_pdf(p)
+view_pdf(i)
