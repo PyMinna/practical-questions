39f917536c53d5188ed1abcdb61d09cfcd4d95ab
PyMinna <kadeejaminnna@gmail.com>
2026-07-15 12:44:06 +0530

    built-in polymorphism examples

---
+++ OOPs Practical/Polymorphism/len_polymorphism.py
@@ -0,0 +1,12 @@
+text = "Hello Minnaa"
+
+fruits = ["banana","stawberry","mango","apple"]
+
+num = {12,22,33,44,55}
+
+letters = ('A','B','C','D','E','F')
+
+print(len(text))
+print(len(fruits))
+print(len(num))
+print(len(letters))
